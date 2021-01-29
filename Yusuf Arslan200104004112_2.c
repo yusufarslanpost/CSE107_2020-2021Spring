d58c52b775f017ef7b7237e0cce55b5700bdee2c
yusufarslanpost <78217370+yusufarslanpost@users.noreply.github.com>
2021-01-29 20:46:08 +0300

    Update Yusuf Arslan200104004112_2.c

--- Yusuf Arslan200104004112_2.c
+++ Yusuf Arslan200104004112_2.c
@@ -2,7 +2,6 @@
 
 /* C program to draw a triangle with *. Yusuf Arslan_200104004112 */
 
-
 int main() {
 	
 	
@@ -18,18 +17,18 @@ int main() {
    {
    	
       for (spc = 1; spc <= rows - i+5 ; ++spc) 
-	  {
+      {
          printf("  ");
       }
       while (k != 2 * i - 1) 
-	  {
+      {
          printf("* ");
          ++k;
       }
       printf("\n");
-      
-	  //k'yi for döngüsünün sonuna yazdým. her döngü baþlangýcýnda k 0'a eþit olsun diye
-	  k=0;
+	   
+      //k'yi for dÃ¶ngÃ¼sÃ¼nÃ¼n sonuna yazdÃ½m. her dÃ¶ngÃ¼ baÃ¾langÃ½cÃ½nda k 0'a eÃ¾it olsun diye
+      k=0;
    }
    return 0;
 }
